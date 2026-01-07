69ee597f4156a1e8d3a55e03b44fb20096354f26
Aline Lima <aline.souza21@outlook.com.br>
2026-01-07 10:26:24 -0300

    add Sentry

--- Ande-projeto/src/main.tsx
+++ Ande-projeto/src/main.tsx
@@ -3,6 +3,24 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './assets/styles/globals.css'
 
+
+// 1. Importe o Sentry
+import * as Sentry from "@sentry/react";
+
+// 2. Inicie o Sentry
+Sentry.init({
+  dsn: import.meta.env.VITE_SENTRY_DSN, // Vamos criar essa variável já já
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, // Em produção real, baixe para 0.1 ou 0.2
+  // Session Replay
+  replaysSessionSampleRate: 0.1, 
+  replaysOnErrorSampleRate: 1.0, 
+});
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
