c22a0edcaf55e87a88cbba24d7c87795248bf960
Aline Lima <aline.souza21@outlook.com.br>
2026-01-07 11:40:31 -0300

    debugando Source maps

--- Ande-projeto/src/services/sentry.ts
+++ Ande-projeto/src/services/sentry.ts
@@ -1,11 +1,16 @@
 import * as Sentry from '@sentry/react'
-import { ENV } from '../config/envs'
+
 
 export function initSentry() {
-  if (ENV.isProd && import.meta.env.VITE_SENTRY_DSN) {
+
+  if (import.meta.env.VITE_SENTRY_DSN) {
     Sentry.init({
       dsn: import.meta.env.VITE_SENTRY_DSN,
       tracesSampleRate: 1.0,
+      debug: true, 
+      environment: 'development' 
     })
+  } else {
+    console.warn("Sentry DSN não encontrado.")
   }
 }
