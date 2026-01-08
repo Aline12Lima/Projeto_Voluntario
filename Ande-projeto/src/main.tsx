0b7df65f359d34bf486550dd8a4affffb2881029
Aline Lima <aline.souza21@outlook.com.br>
2026-01-08 14:18:01 -0300

    Evita iniciar o Sentry duas vezes

--- Ande-projeto/src/main.tsx
+++ Ande-projeto/src/main.tsx
@@ -1,15 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './assets/styles/globals.css'
-
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./assets/styles/globals.css";
 
 // 1. Importe o Sentry
 import * as Sentry from "@sentry/react";
 
 // 2. Inicie o Sentry
 Sentry.init({
-  dsn: import.meta.env.VITE_SENTRY_DSN, // Vamos criar essa variável já já
+  dsn: import.meta.env.VITE_SENTRY_DSN,
   integrations: [
     Sentry.browserTracingIntegration(),
     Sentry.replayIntegration(),
@@ -17,12 +16,12 @@ Sentry.init({
   // Performance Monitoring
   tracesSampleRate: 1.0, // Em produção real, baixe para 0.1 ou 0.2
   // Session Replay
-  replaysSessionSampleRate: 0.1, 
-  replaysOnErrorSampleRate: 1.0, 
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
 });
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-)
+);
--- Ande-projeto/src/services/sentry.ts
+++ Ande-projeto/src/services/sentry.ts
@@ -1,16 +1,5 @@
 import * as Sentry from '@sentry/react'
 
-
-export function initSentry() {
-
-  if (import.meta.env.VITE_SENTRY_DSN) {
-    Sentry.init({
-      dsn: import.meta.env.VITE_SENTRY_DSN,
-      tracesSampleRate: 1.0,
-      debug: true, 
-      environment: 'development' 
-    })
-  } else {
-    console.warn("Sentry DSN não encontrado.")
-  }
+export function captureError(error: unknown) {
+  Sentry.captureException(error)
 }
